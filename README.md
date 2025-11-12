d45616968206f649c56b4a7be69bd36f913f4e3c
AhmedBakrElsaid <a.bakr2472001@gmail.com>
2025-11-12 16:31:56 +0200

    two madrabs and ball,madrab01 move to up window

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 # we setup screen for Game
 
 ![alt text](image.png)
+
+# we prepred two madrabs(madrab01,madrab02) and ball .
+# we prepared function to move madrab01 to up window.
+
+![alt text](image-1.png)
+
--- app.py
+++ app.py
@@ -7,5 +7,43 @@ window.bgcolor("black")
 window.setup(width=800, height=600)
 window.tracer(0)
 
+
+madrab01 = turtle.Turtle()
+madrab01.speed(0)
+madrab01.shape("square")
+madrab01.color("blue")
+madrab01.shapesize(stretch_wid=5,stretch_len=1)
+madrab01.penup()
+madrab01.goto(-350,0)
+
+
+madrab02 = turtle.Turtle()
+madrab02.speed(0)
+madrab02.shape("square")
+madrab02.color("red")
+madrab02.shapesize(stretch_wid=5,stretch_len=1)
+madrab02.penup()
+madrab02.goto(350,0)
+
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+
+#functions
+def madrab01_up():
+    y = madrab01.ycor()
+    y += 20
+    madrab01.sety(y)
+
+#keyboard bindings
+
+window.listen()
+window.onkeypress(madrab01_up,'w')
+
+
 while True:
     window.update()
